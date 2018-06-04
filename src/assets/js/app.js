db5588d212beb19fa7ea0efded933a931658db88
Исаев Алексей Витальевич <anxieter@yandex.ru>
2018-06-04 18:08:31 +0300

    Остальные страницы + JS

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,12 +1,13 @@
-import $ from 'jquery';
-import whatInput from 'what-input';
+import $ from "jquery";
+import whatInput from "what-input";
 
 window.$ = $;
 
-import Foundation from 'foundation-sites';
+// import Foundation from "foundation-sites";
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
-//import './lib/foundation-explicit-pieces';
-
+import "./lib/foundation-explicit-pieces";
 
 $(document).foundation();
+
+// Foundation.Abide.defaults.patterns["dashes_only"] = /^[0-9-]*$/;
